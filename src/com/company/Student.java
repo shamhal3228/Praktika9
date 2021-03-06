8842c0e3456535ee9847a2c103b21ed290bec0b2
Shamhal <boss.shamhal@mail.ru>
2021-03-06 22:10:05 +0300

    Added another print(Hello World) in Student function

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -14,6 +14,7 @@ public class Student {
         this.FIO=FIO;
         this.id=id;
         System.out.print("Hello world");
+        System.out.print("Hello world");
     }
 
     public int getId()
